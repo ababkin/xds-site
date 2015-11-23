31064be01de34125fc592467d6cfe96714f3667c
Alex Babkin <ababkin@sumall.com>
2015-11-23 17:28:45 -0500

    works, but not for https

--- src/Forms/Source.hs
+++ src/Forms/Source.hs
@@ -5,7 +5,7 @@
 module Forms.Source where
 
 import Control.Applicative ((<$>), (<*>), (<|>), pure)
-import Control.Exception (catch)
+import Control.Exception (catch, SomeException)
 import Control.Monad.Catch (MonadThrow, MonadCatch, Exception)
 import Control.Monad.IO.Class (liftIO, MonadIO)
 import Data.Maybe (isJust)
@@ -16,11 +16,15 @@ import Data.UUID (UUID)
 import Data.Time.Clock (getCurrentTime, UTCTime)
 import Text.Digestive (Form)
 import Text.Digestive.Form ((.:), text, optionalText, check, checkM, validateM, file)
-import Text.Digestive.Types (Result(Success, Error))
+import qualified Text.Digestive.Types as D (Result(Success, Error))
 import Network.HTTP.Client (HttpException, parseUrl)
-import Network.HTTP (simpleHTTP, getRequest, getResponseCode)
+import Network.HTTP (simpleHTTP, getRequest, getResponseCode, rspCode, Response)
 import Network.HTTP.Headers (findHeader, HeaderName(HdrLocation))
+import qualified Network.Stream as NS
 import System.Posix (fileSize, getFileStatus)
+import Network.TCP (HStream)
+import qualified Data.ByteString.Lazy as L
+
 
 import Application (AppHandler)
 import Forms.Validators (isFormattedLikeURL, isNotEmpty)
@@ -48,13 +52,13 @@ sourceForm uid =
                           <*> "local"   .: file 
 
 
-      where extractUrls :: (Maybe Text, Maybe Text, Maybe FilePath) -> AppHandler (Result Text (Maybe Text, Maybe Text))
+      where extractUrls :: (Maybe Text, Maybe Text, Maybe FilePath) -> AppHandler (D.Result Text (Maybe Text, Maybe Text))
             
             extractUrls (Nothing, Nothing, Nothing) = 
-              return $ Error "Please enter Webpage URL, Dataset URL or upload a Dataset file"
+              return $ D.Error "Please enter Webpage URL, Dataset URL or upload a Dataset file"
 
             extractUrls (maybeWebsiteUrl, maybeRemoteDatasetUrl, Nothing) = 
-              return $ Success (maybeWebsiteUrl, maybeRemoteDatasetUrl)
+              return $ D.Success (maybeWebsiteUrl, maybeRemoteDatasetUrl)
             
             extractUrls (maybeWebsiteUrl, maybeRemoteDatasetUrl, Just path) = do
               size <- fileSize <$> liftIO (getFileStatus path)
@@ -62,12 +66,12 @@ sourceForm uid =
                 then extractUrls (maybeWebsiteUrl, maybeRemoteDatasetUrl, Nothing)
                 else
                   if isJust maybeRemoteDatasetUrl 
-                    then return $ Error "Please either enter a remote URL OR attach a file, not both"
+                    then return $ D.Error "Please either enter a remote URL OR attach a file, not both"
                     else do
                       {- store <- use filestore -}
                       {- url <- storeFile store path Nothing -}
                       liftIO . putStrLn $ "filepath received: " ++ show path
-                      return $ Success (maybeWebsiteUrl, Just "http://s3.com/somepath")
+                      return $ D.Success (maybeWebsiteUrl, Just "http://s3.com/somepath")
 
 
 
@@ -77,26 +81,29 @@ sourceForm uid =
 
     isPingable 
       :: Maybe Text 
-      -> IO (Result Text (Maybe Text))
-    isPingable Nothing = return $ Success Nothing 
+      -> IO (D.Result Text (Maybe Text))
+    isPingable Nothing = return $ D.Success Nothing 
     isPingable (Just url) = do
-      {- resp <- liftIO $ catch (simpleHTTP (getRequest $ T.unpack url) ) giveUp -}
-      result <- simpleHTTP (getRequest $ T.unpack url)
-      code <- liftIO $ catch (getResponseCode resp) giveUp
-      print code
-      case code of
-        (2,_,_) -> return . Success $ Just url
-        (3,_,_) ->
-          case result of
-            Left _err -> return pingError
-            Right resp ->
+      result <- catchAny ( simpleHTTP (getRequest $ T.unpack url)  ) giveUp 
+      case result of
+        Left _err -> return pingError
+        Right resp ->
+          case rspCode resp of
+            (2,_,_) -> return . D.Success $ Just url
+            (3,_,_) ->
               case findHeader HdrLocation resp of
                 Nothing -> return pingError
-                Just location -> isPingable . Just $ T.pack location
+                Just location -> return . D.Error $ "URL redirects to: " <> T.pack location <> " - consider using this URL instead"
         _       -> return pingError 
       where
-        giveUp :: (MonadThrow m) => HttpException -> m (Int, Int, Int)
-        giveUp = const $ return (5,0,0)
+        catchAny :: IO a -> (SomeException -> IO a) -> IO a
+        catchAny = Control.Exception.catch
 
-        pingError = Error "Could not successfully ping the URL"
+        giveUp :: SomeException -> IO (Either NS.ConnError (Response String))
+        giveUp = const . return . Left $ NS.ErrorMisc pingErrorMessage
+
+        pingError = D.Error $ T.pack pingErrorMessage
+        
+        pingErrorMessage = "Could not successfully ping the URL"
+
 
